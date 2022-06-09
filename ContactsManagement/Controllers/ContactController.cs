a8c2070fe441351b39b73667a719f03ccfb30a32
cristinevsousa <cristinevsousa@gmail.com>
2022-06-08 22:33:21 -0300

    first commit

---
+++ ContactsManagement/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsManagement.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult ConfirmDelete()
+        {
+            return View();
+        }
+    }
+}
---
+++ ContactsManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContactsManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
